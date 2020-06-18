f8650bea703964a0786b8dabb5c9682505af8dd8
Woozie15 <gus.inkotte@gmail.com>
2020-06-17 21:35:16 -0300

    first commit

---
+++ src/database/db.js
@@ -0,0 +1,6 @@
+const sqlite3 = require("sqlite3").verbose()
+
+const db = new sqlite3.Database("./src/database/database.db")
+
+module.exports = db
+
---
+++ src/server.js
@@ -0,0 +1,89 @@
+const express = require("express")
+const server = express()
+
+const db = require("./database/db.js")
+
+server.use(express.static("public"))
+server.use(express.urlencoded({extended: true}))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true
+})
+
+
+// Req = Requisição
+// Res = Resposta
+server.get("/", (req, res) => {
+  res.render("index.html", {
+    title: "Um título"
+  })
+})
+
+server.get("/create-point", (req, res) => {
+
+  res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) => {
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items,
+  ]
+
+  function afterInsertData(err) {
+    if (err) {
+      return console.log(err)
+    }
+    console.log("Cadastrado com sucesso")
+    console.log(this)
+    return res.render("create-point.html", {saved:true})
+  }
+
+  db.run(query, values, afterInsertData)
+
+})
+
+
+
+server.get("/search", (req, res) => {
+
+  const search = req.query.search
+
+  if(search == ""){
+    return res.render("search-results.html", {total: 0})
+  }
+
+
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+    if (err) {
+      return console.log(err)
+    }
+
+    const total = rows.length
+
+    return res.render("search-results.html", { places: rows, total: total })
+  })
+
+})
+
+
+server.listen(3000)
